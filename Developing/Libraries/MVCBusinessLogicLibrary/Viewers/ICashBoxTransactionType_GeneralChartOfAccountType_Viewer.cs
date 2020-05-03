ea6053f7b3deb5c64ed5d8b241e0fd44695608d4
rayad.ramez <ayad.ramez@gmail.com>
2020-05-03 06:22:10 +0200

    cv bcvb

---
+++ Developing/Libraries/MVCBusinessLogicLibrary/Viewers/ICashBoxTransactionType_GeneralChartOfAccountType_Viewer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MerkDataBaseBusinessLogicProject;
+
+namespace MVCBusinessLogicLibrary.Viewers
+{
+	public interface ICashBoxTransactionType_GeneralChartOfAccountType_Viewer : IViewer
+	{
+		List<CashBoxTransactionType_GeneralChartOfAccountType_cu> List_CashBoxTransactionType_GeneralChartOfAccountType { get; set; }
+	}
+}
