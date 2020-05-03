d208a0f7432eb1cef34c915505eb1abdf76f2510
rayad.ramez <ayad.ramez@gmail.com>
2020-05-03 06:40:22 +0200

    fgv

---
+++ Developing/MerkSolutions/Dental_EMR/Form1.cs
@@ -0,0 +1,18 @@
+﻿using CommonControlLibrary;
+using CommonUserControls.PEMRCommonViewers;
+
+namespace Dental_EMR
+{
+	public partial class Form1 : DevExpress.XtraEditors.XtraForm
+	{
+		private PEMRContainer pemr;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			CommonViewsActions.ShowUserControl(ref pemr, this);
+			//_paintViewer.Initialize(null);
+		}
+	}
+}
