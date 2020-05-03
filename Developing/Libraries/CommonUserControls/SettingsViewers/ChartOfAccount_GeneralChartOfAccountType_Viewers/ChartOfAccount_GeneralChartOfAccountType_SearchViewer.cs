ec9bb3aa7856f04ef8dcfc541bfa85a375af4ba2
rayad.ramez <ayad.ramez@gmail.com>
2020-05-03 06:06:17 +0200

    vbcvb

---
+++ Developing/Libraries/CommonUserControls/SettingsViewers/ChartOfAccount_GeneralChartOfAccountType_Viewers/ChartOfAccount_GeneralChartOfAccountType_SearchViewer.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CommonUserControls.SettingsViewers.ChartOfAccount_GeneralChartOfAccountType_Viewers
+{
+	public partial class ChartOfAccount_GeneralChartOfAccountType_SearchViewer : UserControl
+	{
+		public ChartOfAccount_GeneralChartOfAccountType_SearchViewer()
+		{
+			InitializeComponent();
+		}
+	}
+}
