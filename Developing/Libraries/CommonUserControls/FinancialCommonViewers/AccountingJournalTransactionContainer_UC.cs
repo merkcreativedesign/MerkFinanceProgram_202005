adb49ad3024b99629e8d20d7230e2bbdedb1272c
rayad.ramez <ayad.ramez@gmail.com>
2020-05-03 05:37:24 +0200

    dfgcvxv

---
+++ Developing/Libraries/CommonUserControls/FinancialCommonViewers/AccountingJournalTransactionContainer_UC.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CommonUserControls.FinancialCommonViewers
+{
+	public partial class AccountingJournalTransactionContainer_UC : UserControl
+	{
+		public AccountingJournalTransactionContainer_UC()
+		{
+			InitializeComponent();
+		}
+	}
+}
